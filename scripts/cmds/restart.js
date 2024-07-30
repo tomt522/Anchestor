8f1d3bc04cb1667fc89111555b46cde6519416f9
Easir Iqbal Mahi <136728953+EASIRMAHI@users.noreply.github.com>
2024-07-31 02:58:42 +0600

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -35,7 +35,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted Done Na Master Loid\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | Bot restarted Done \n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
